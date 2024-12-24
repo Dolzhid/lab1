07d952ff1331731d5e3030dabb5e2a01cd0831d3
Dolzhid <pronteevadolzid@gmail.com>
2024-12-24 03:29:07 +0300

    Add files via upload

---
+++ check.cpp
@@ -0,0 +1,20 @@
+#include "check.h" 
+float check() {
+	float value;
+	while (true) {
+		cin >> value;
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(),
+				'\n');
+		число: "; 
+			'\n');
+		}
+	}
+	Файл figur.h:
+	cout << "Некорректный ввод! Пожалуйста, введите 
+}
+else {
+	cin.ignore(numeric_limits<streamsize>::max(),
+		return value;
+}
---
+++ figur.cpp
@@ -0,0 +1,13 @@
+#include "figur.h" 
+Figur::Figur() {
+	cout << "Вызван конструктор класса 
+		Figur\n"; }; 
+		Figur::Figur(const int& n) {
+		cout << "Вызван конструктор c 
+			параметрами класса Figur\n"; } 
+			Figur::Figur(const Figur & n) {
+			cout << "Вызван конструктор 
+				копирования класса Figur\n"; }; 
+				Figur::~Figur() {
+				cout << "Вызван деструктор класса 
+					Figur\n"; };
